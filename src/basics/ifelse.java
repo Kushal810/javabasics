b9424064668f9f53ff05023c029450d5572a298e
kushal <kush612006@gmail.com>
2026-08-17 15:20:20 +0530

    Basics

---
+++ src/basics/ifelse.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class ifelse {
+    public static void main(String[]args)
+    {
+        int marks=75;
+        if(marks > 60)
+        {
+            System.out.println("You are distintion pass");
+        }
+        else
+        {
+            System.out.println("Your firstclass");
+        }
+    }
+}
+
