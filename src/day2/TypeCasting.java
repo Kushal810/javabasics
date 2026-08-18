619a3da9e270ac80db4fa1fbc2e778bf5d1cf21a
kushal <kush612006@gmail.com>
2026-08-18 09:51:07 +0530

    Basics

--- src/day2/TypeCasting.java
+++ src/day2/TypeCasting.java
@@ -7,6 +7,7 @@ public class TypeCasting {
         double value=num;
         System.out.println(value);
         double price=99.99;
+        System.out.println("The Round off"+Math.round(price));
         int converted=(int)price;
         System.out.println(converted);
 
