31601844054515939d6ef763c923d771e26984e5
kushal <kush612006@gmail.com>
2026-08-18 11:42:41 +0530

    Basics

---
+++ src/day2/LOOPS_PATTERNS/patternsquare.java
@@ -0,0 +1,12 @@
+package day2.LOOPS_PATTERNS;
+
+public class patternsquare {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
