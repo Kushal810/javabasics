804a9af90581121be20643bb5f6e7721a237f301
kushal <kush612006@gmail.com>
2026-08-18 09:26:19 +0530

    Basics

---
+++ src/day2/TypeCasting.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class TypeCasting {
+    public static void main(String[] args)
+    {
+        int num=100;
+        double value=num;
+        System.out.println(value);
+        double price=99.99;
+        int converted=(int)price;
+        System.out.println(converted);
+
+    }
+}
