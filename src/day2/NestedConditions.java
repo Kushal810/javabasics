ea342e07ccaa39b733ad5a699a8ebfcfa79c464f
kushal <kush612006@gmail.com>
2026-08-18 10:07:17 +0530

    Basics

---
+++ src/day2/NestedConditions.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class NestedConditions {
+    public static void main(String[] args)
+    {
+        int marks=85;
+        if(marks >=40)
+        {
+            if(marks <=95)
+            {
+                System.out.println("Secured Distinction");
+            }
+        }
+    }
+}
