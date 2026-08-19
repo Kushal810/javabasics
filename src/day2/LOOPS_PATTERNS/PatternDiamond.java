9e57cce627661982d6d7e4cacc040317c7cbc2f7
kushal <kush612006@gmail.com>
2026-08-19 09:04:05 +0530

    Basics

--- src/day2/LOOPS_PATTERNS/PatternDiamond.java
+++ src/day2/LOOPS_PATTERNS/PatternDiamond.java
@@ -5,14 +5,14 @@ public class PatternDiamond {
 
         int rows = 5;
 
-        // Upper half
+
         for (int i = 1; i <= rows; i++) {
 
-            // Spaces
+
             for (int s = 1; s <= rows - i; s++)
                 System.out.print(" ");
 
-            // Stars
+
             for (int j = 1; j <= (2 * i - 1); j++)
                 System.out.print("*");
 
@@ -22,11 +22,11 @@ public class PatternDiamond {
         // Lower half
         for (int i = rows - 1; i >= 1; i--) {
 
-            // Spaces
+
             for (int s = 1; s <= rows - i; s++)
                 System.out.print(" ");
 
-            // Stars
+
             for (int j = 1; j <= (2 * i - 1); j++)
                 System.out.print("*");
 
