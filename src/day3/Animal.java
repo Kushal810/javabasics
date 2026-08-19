d2ca28de69de03dfc119d3781e7216ce92571995
kushal <kush612006@gmail.com>
2026-08-19 13:09:05 +0530

    Basics

---
+++ src/day3/Animal.java
@@ -0,0 +1,27 @@
+package day3;
+
+public class Animal {
+    void eat() {
+        System.out.println("Dog Eats");
+    }
+
+
+
+    static class Dog extends Animal {
+        void bark() {
+            System.out.println("Dog Barks");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();   // inherited from Animal
+        d.bark();
+    }
+}
+
+
+
+
